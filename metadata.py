3cf7c4cbf219cd818676cc411eda16b0d1db7b45
jean <kaliregenold@yahoo.com>
2020-09-05 18:54:02 -0400

    Add file for usesr specific metadata.

---
+++ metadata.py
@@ -0,0 +1,7 @@
+# This file to be filled out by user.
+
+# data_path is the path to the directory containing the chord change data
+data_path = ""
+# start_date is the first date of practice
+# Follow the form %Y.%m.%d eg. 2020.08.24
+start_date = ""
